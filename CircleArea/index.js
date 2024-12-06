4515d48487f8da080b84490f048bea2938261280
Kadircan <113523365+BigNjapan@users.noreply.github.com>
2024-12-06 23:26:49 +0300

    Add files via upload

---
+++ CircleArea/index.js
@@ -0,0 +1,9 @@
+
+const { circleArea, circleCircumference } = require('./circle');
+
+
+const radius = 5;
+
+
+console.log(`Daire Alanı: ${circleArea(radius)}`);
+console.log(`Daire Çevresi: ${circleCircumference(radius)}`); 
